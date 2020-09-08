8ce2c51a8248158b73e219c5c99e199a290a10fc
vladimir-siv <vladimir.sivcev@gmail.com>
2020-09-08 17:26:44 +0200

    Medium terrain change

--- src/ForrestGumpAI/Assets/Scripts/Logic/Terrain/MediumTerrain.cs
+++ src/ForrestGumpAI/Assets/Scripts/Logic/Terrain/MediumTerrain.cs
@@ -21,9 +21,7 @@ public class MediumTerrain : ITerrainGenerator
 			case 2:
 				var spread = ObjectActivator.Construct<SpreadPathway>();
 				spread.Scale = Random.Range(15f, 20f);
-				if (connectorAngle >= 25f) spread.Opened = (SpreadPathway.Gate)(int)Mathf.Pow(2, Random.Range(0, 2));
-				else if (connectorAngle <= -25f) spread.Opened = (SpreadPathway.Gate)(int)Mathf.Pow(2, Random.Range(1, 3));
-				else spread.Opened = (SpreadPathway.Gate)(int)Mathf.Pow(2, Random.Range(0, 3));
+				spread.Opened = SpreadPathway.Gate.North;
 				return spread;
 		}
 

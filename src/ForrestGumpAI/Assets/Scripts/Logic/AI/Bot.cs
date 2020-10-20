0d31b0f6d15cac522745389a648c241d10116c49
vladimir-siv <vladimir.sivcev@gmail.com>
2020-10-20 19:21:11 +0200

    GI Library update

--- src/ForrestGumpAI/Assets/Scripts/Logic/AI/Bot.cs
+++ src/ForrestGumpAI/Assets/Scripts/Logic/AI/Bot.cs
@@ -13,7 +13,7 @@ public class Bot
 		{
 			if (prototype == null)
 			{
-				prototype = new NeuralBuilder(Inputs);
+				prototype = new NeuralBuilder(Shape.As2D(1u, Inputs));
 				prototype.FCLayer(8u, ActivationFunction.ELU);
 				prototype.FCLayer(4u, ActivationFunction.ELU);
 				prototype.FCLayer(3u, ActivationFunction.Sigmoid);

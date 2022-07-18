b4e21f6127bb7d995df99bf028cd42a2a24e156b
vladimir-siv <vladimir.sivcev@gmail.com>
2022-07-18 18:55:47 +0200

    Random Agent

--- src/ForrestGumpAI/Assets/Scripts/Logic/AI/Agent.cs
+++ src/ForrestGumpAI/Assets/Scripts/Logic/AI/Agent.cs
@@ -1,74 +1,17 @@
-﻿using System;
-using UnityEngine;
-using GrandIntelligence;
+﻿using UnityEngine;
 
 public class Agent
 {
-	public const uint Inputs = 5u;
-	public const uint Outputs = 3u;
-
-	private readonly float[] sensors = new float[Inputs];
-	private readonly float[] outputs = new float[Outputs];
-
 	private Runner runner = null;
-	private float score = 0f;
-
-	public BasicBrain Brain { get; set; } = null;
 
 	public void AssignAgent(Runner runner)
 	{
 		this.runner = runner;
-		runner.PathwayEntered += pathway => score += pathway.Difficulty;
-		runner.RunnerDeath += RunnerDeath;
-		for (var i = 0; i < sensors.Length; ++i) sensors[i] = 0f;
-		score = 0f;
 	}
 
 	public void Think()
 	{
-		RefreshSensors();
-
-		Brain.NeuralNetwork.Input.Transfer(sensors);
-		Brain.NeuralNetwork.Eval();
-		Brain.NeuralNetwork.Output.Retrieve(outputs);
-
-		var decision = outputs.ArgMax() - 1;
+		var decision = Random.Range(0, 3) - 1;
 		if (decision != 0) runner.Steer(decision);
-	}
-
-	private void RefreshSensors()
-	{
-		const float MaxRayDistance = 10f;
-
-		Vector3 direction(int i)
-		{
-			switch (i)
-			{
-				case 0: return -runner.transform.right;
-				case 1: return (runner.transform.forward - runner.transform.right).normalized;
-				case 2: return runner.transform.forward;
-				case 3: return (runner.transform.forward + runner.transform.right).normalized;
-				case 4: return runner.transform.right;
-				default: throw new ArgumentException($"Invalid direction: {i}.");
-			}
-		}
-
-		for (var i = 0; i < Inputs; ++i)
-		{
-			var hit = runner.Raycast(direction(i), MaxRayDistance);
-			if (hit != null) sensors[i] = hit.Value.distance / MaxRayDistance;
-			else sensors[i] = 1f;
-		}
-	}
-
-	private void RunnerDeath()
-	{
-		var distance = Vector3.Distance(runner.transform.position, runner.LastPathway.ExitPoint);
-		var time = (float)runner.TimeSinceSpawned.TotalSeconds;
-
-		var reward = Mathf.Pow(score / .17e3f, 4f);
-		var penalty = Mathf.Pow(distance / .54e3f, 4f) * time;
-
-		Brain.EvolutionValue = Mathf.Max(reward - penalty, float.Epsilon);
 	}
 }
--- src/ForrestGumpAI/Assets/Scripts/Logic/AI/Simulation.cs
+++ src/ForrestGumpAI/Assets/Scripts/Logic/AI/Simulation.cs
@@ -1,57 +1,22 @@
-﻿using GrandIntelligence;
-
-public class Simulation
+﻿public class Simulation
 {
-	private Agent[] agents = null;
-	private DarwinBgea evolution = null;
-
 	public void Begin(Agent[] agents)
 	{
-		this.agents = agents;
 
-		var prototype = new NeuralBuilder(Shape.As2D(1u, Agent.Inputs));
-		prototype.FCLayer(8u, ActivationFunction.ELU);
-		prototype.FCLayer(4u, ActivationFunction.ELU);
-		prototype.FCLayer(Agent.Outputs, ActivationFunction.Sigmoid);
-
-		var firstGen = new Population((uint)agents.Length);
-		for (var i = 0; i < agents.Length; ++i)
-		{
-			var brain = new BasicBrain(prototype);
-			brain.Randomize(-1.0f, +1.0f, Distribution.Uniform);
-			firstGen.Add(brain);
-		}
-
-		evolution = new DarwinBgea
-		(
-			firstGen,
-			Selection.RandFit(1u),
-			BasicBrain.SequentialEvenCrossover(firstGen.Size, ((BasicBrain)firstGen[0u]).NeuralNetwork.Params),
-			generations: 1000u,
-			mutation: 10.0f
-		);
-
-		prototype.Dispose();
 	}
 
 	public void EpisodeStart()
 	{
-		for (var i = 0; i < agents.Length; ++i)
-		{
-			agents[i].Brain = (BasicBrain)evolution.Generation[(uint)i];
-		}
+
 	}
 
 	public void RunnerTerminated(int runnersLeft)
 	{
-		if (runnersLeft == 0)
-		{
-			evolution.Cycle();
-		}
+
 	}
 
 	public void End()
 	{
-		evolution?.Dispose();
+
 	}
 }
